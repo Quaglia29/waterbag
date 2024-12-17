8c50dd60d87c1e2c38cc60742577cc4ed605b63f
Quaglia29 <f.quaglia17@gmail.com>
2024-12-17 11:24:33 +0100

    Update WEBAPP_Waterbag.py

--- WEBAPP_Waterbag.py
+++ WEBAPP_Waterbag.py
@@ -153,7 +153,7 @@ translations = {
 	"size": "size",
 	"tot": "Total",
 	"single_volume": "Single volume"
-}
+	}
 }
 
 # Selettore lingua
@@ -357,7 +357,8 @@ with col3:
             volume_totale += volume_per_bag * count
             
             # Mostra i dettagli del waterbag nel riepilogo
-            st.write(f"{waterbag}: {count} pezzi {t('prize_u')} €{prezzo_unitario:.2f}, {t('tot')}: €{costo_totale:.2f}, {t('single_volume')}: {volume_per_bag:.2f} l")
+            st.write(f"{waterbag}: {count} pezzi ({t('prize_u')} €{prezzo_unitario:.2f}, {t('tot')}: €{costo_totale:.2f}, {t('single_volume')}: {volume_per_bag:.2f} l)")
+	    
 
         # Mostra il prezzo totale alla fine
         st.markdown(f"### {t("total_price")}: €{prezzo_totale:.2f}")
